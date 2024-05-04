e96b74bef34cc9f49804cf85233e8394d1590dd7
sathajsjsh <165791076+sathajsjsh@users.noreply.github.com>
2024-05-04 10:42:11 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+export default function Footer(){
+    return(
+        <div className="footer container">
+        <div className="footer-section">
+            <p className="title">FoodiesHub.com</p>
+            <p>FoodiesHub is a place where you can please your soul and tummy with delicious food recipies of all cuisine. And our service is absolutely free.</p>
+            <p>&copy; 2024 | All Rights Reserved</p>
+        </div>
+        <div className="footer-section">
+            <p className="title">Contact Us</p>
+            <p>foodieshub@gmail.com</p>
+            <p>+91-9361176163</p>
+            <p>2393 Street NYC</p>
+        </div>
+        <div className="footer-section">
+            <p className="title">Socials</p>
+            <p>Facebook</p>
+            <p>Twitter</p>
+            <p>Instagram</p>
+        </div>
+    </div>
+    )
+}
